66ea87daed6603d5f6e85e879e794e3ce8171912
petlitsky <rpetlisky@gmail.com>
2025-09-22 20:04:43 +0300

    Update gt.js

--- gt.js
+++ gt.js
@@ -6,6 +6,10 @@ function getText() {
   const textValue = inputElement.value;
 
   if (textValue === "103122") document.getElementById("output").textContent = "Ограждения рельсов на 20 вагоне: hwtTtNpls5:3/V/3dxiRsgkR.ay1a/ndd/eyxb.";
+  if (textValue === "bublegum") document.getElementById("output").textContent = "yHIxRbj2";
+  if (textValue === "3301") document.getElementById("output").textContent = "https://disk.yandex.by/d/LsLkbNKniB0luw";
+  if (textValue === "123321") document.getElementById("output").textContent = "https://disk.yandex.by/d/9J043UxqKFir-w";
+  if (textValue === "hjskslfhfcsd") document.getElementById("output").textContent = "https:/vetlitszy.jithub.iu/zvuj.html";
 
   // Или просто выводим в консоль:
   console.log(textValue);
